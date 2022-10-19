a6f30f623e062dd30f374f5ca4152c441e3991ea
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-10-19 17:30:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
-	echo "Hello";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>E-Commerce Labs</title>
+</head>
+<body>
+<button>Lab 1</button>
+</body>
+</html>
 
 	
