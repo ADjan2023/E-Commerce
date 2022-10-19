0fb61ceb58814d41a1704c28387a37f2df782ee2
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-10-19 23:13:07 +0000

    db cred

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,9 @@
 <?php
 //Database credentials
-define("SERVER", "localhost");
-define("USERNAME","root");
-define("PASSWD", "");
-define("DATABASE", "ecommercelab");
+define("SERVER", "eu-cdbr-west-03.cleardb.net");
+define("USERNAME","b4799e4ad3c3f6");
+define("PASSWD", "85ebf725");
+define("DATABASE", "heroku_2f28a549a2aad87");
 
 
 ?>
